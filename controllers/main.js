74086ecbeb5110cd21023684a4a2b2006c018fcb
IslamHegazy7196 <islamfathihegazy04@gmai.com>
2022-04-26 22:34:28 +0200

    dashboard

---
+++ controllers/main.js
@@ -0,0 +1,26 @@
+
+const customAPIError=require('../errors/custom-error')
+const jwt=require('jsonwebtoken')
+
+const login = (req, res) => {
+    const {username,password}=req.body
+    if(!username||!password){
+        throw new customAPIError('please provise username and password',400)
+    }
+const id =new Date().getDate()
+const token=jwt.sign({id,username},process.env.JWT_TOKEN,{expiresIn:'30d'})
+  res.status(200).json({msg:'user created',token});
+};
+
+const dashboard = (req, res) => {
+    
+  const luckyNumber = Math.floor(Math.random() * 1000);
+  res
+    .status(200)
+    .json({
+      msg: `hello, ${req.user.username}`,
+      secret: `here is your lucky number ${luckyNumber}`,
+    });
+};
+
+module.exports = { login, dashboard };
---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+const customAPIError=require('../errors/custom-error')
+const jwt=require('jsonwebtoken')
+
+const authMiddleware=(req,res,next)=>{
+    const authHeader=req.headers.authorization
+    if(!authHeader||!authHeader.startsWith('Bearer')){
+        throw new customAPIError('no token provided',401)
+    }
+    const token =authHeader.split(' ')[1]
+    try {
+        const decoded=jwt.verify(token,process.env.JWT_TOKEN)
+        const {id,username}=decoded
+        req.user={id,username}
+        next()
+    } catch (error) {
+        throw new customAPIError('not authorized to access this route',401)
+    }
+}
+
+module.exports=authMiddleware
